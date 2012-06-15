c34fb6f922ab60535ee073b1fe81fb283abb0ee2
Polo <rafael.polo@gmail.com>
2012-06-15 02:20:08 -0300

    ajustes mil

--- app/assets/javascripts/webdoc.js.coffee
+++ app/assets/javascripts/webdoc.js.coffee
@@ -1,2 +1,13 @@
 $ ->
 	$("iframe").load -> $(this).fadeIn(600)
+
+	$('.box-head').hover(
+		-> 
+			$(this).children().css("color", "#fe8a2d")
+			$(this).children().next().css("color", "#fe8a2d")
+		-> 
+			$(this).children().css("color", "#b96b3b")
+			$(this).children().next().css("color", "#b96b3b")
+	)
+
+	$(".aberto").rotate(90) if $(".aberto").length
---
+++ config/routes (Rafael Polo's conflicted copy 2012-06-05).rb
@@ -0,0 +1,22 @@
+Webdoc::Application.routes.draw do
+  resources :georefs
+  resources :ensaios
+  resources :tags
+  resources :capitulos
+  resources :episodios
+
+  match "/videos" => "pages#videos"
+  match "/mapa" => "pages#mapa"
+  match "/info" => "pages#info"
+
+  match "/player" => "pages#player"
+
+  match "/admin" => "admin#episodios"
+	match "/admin/galerias" => "admin#galerias"
+	match "/admin/arquivos" => "admin#arquivos"
+	match "/admin/blog" => "admin#blog"
+	match "/admin/biografia" => "admin#biografia"
+	match "/admin/contatos" => "admin#contatos"
+  
+  root :to => 'pages#index'
+end
