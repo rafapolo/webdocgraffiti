5dfe7be87cd7679e226040056aad0e729d8eff65
Rafael Polo <rafael.polo@gmail.com>
2012-06-28 01:22:45 -0300

    +admin

--- app/models/episodio.rb
+++ app/models/episodio.rb
@@ -1,6 +1,14 @@
 class Episodio < ActiveRecord::Base
   default_scope :order => 'created_at DESC'
 
+	has_attached_file :capa,
+		:styles => {
+	   		:thumb=> "200x"
+	   	},
+		:storage => :s3,
+		:s3_credentials => "#{Rails.root}/config/s3.yml",
+		:path => "/:class/:style/:id/:filename"
+
   has_many :blocos, :dependent => :destroy  
   
   validates_presence_of :titulo, :sinopse
