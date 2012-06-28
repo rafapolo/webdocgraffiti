e5c86f530bd8b10fde3cdbf21a335a9d2a8be789
Polo <rafael.polo@gmail.com>
2012-06-28 15:09:52 -0300

    sem paperclip

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-Paperclip.options[:command_path] = "~/Downloads/ImageMagick-6.7.7/bin"
-
 # Initialize the rails application
 Webdoc::Application.initialize!
