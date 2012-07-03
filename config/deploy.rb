a0cd26c450bce4abb7cf1b09b3477244c0f62339
Rafael Polo <rafael.polo@gmail.com>
2012-07-03 01:45:33 -0300

    railsplayground fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,6 @@
 default_run_options[:pty] = true
+set :use_sudo, false
+
 set :repository, "git@github.com:rafapolo/webdocgraffiti.git"  # Your clone URL
 set :scm, "git"
 set :user, "webdocgr"  # The server's user for deploys
@@ -9,14 +11,16 @@ set :branch, "master"
 ssh_options[:forward_agent] = true
 set :deploy_via, :remote_cache
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+role :web, "webdocgraffiti.com.br"
+role :app, "webdocgraffiti.com.br"
+set :deploy_to, "/home/#{user}/#{application}" 
+set :runner, "webdocgr"
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+task :restart, :roles => :app do
+end
+
+after "deploy:update_code", :roles => [:web, :db, :app] do
+  run "chmod 755 #{release_path}/public -R" 
+end
+
+after "deploy:update", "deploy:cleanup" 
