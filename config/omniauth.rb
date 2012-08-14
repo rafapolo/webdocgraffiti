327f74d09ed98e7338f329f3065e484093759334
Polo <rafael.polo@gmail.com>
2012-08-14 17:01:40 -0300

    FB + scope para poster no wall

--- config/omniauth.rb
+++ config/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '380422978692298', 'fa0f216f04559ca412377b4754f41acb', {:client_options => {:ssl => {:verify => false}}, 
-  :scope => 'user_about_me'}
+  :scope => 'user_about_me, publish_stream'}
 end
