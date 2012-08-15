a80a36fe38c34a63e43ec4c9f6a4bf372b2884a9
rafapolo <rafael.polo@gmail.com>
2012-08-14 23:00:01 -0300

    fix hiperbalões

--- app/models/hiperbalon.rb
+++ app/models/hiperbalon.rb
@@ -3,6 +3,7 @@ class Hiperbalon < ActiveRecord::Base
 	belongs_to :marcador
 	validates_presence_of :in, :out, :bloco_id, :url, :image_file_name
 
-	has_attached_file :image,
-	  :styles => {:thumb=> "x150"}
+	has_attached_file :image, :styles => {:thumb=> "x150"},
+		:url => "/system/:class/:id/:style/:basename.:extension",
+		:path => ":rails_root/public/system/:class/:id/:style/:basename.:extension"
 end
